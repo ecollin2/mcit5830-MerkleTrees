ac59b91812b591b088b22918154bec1091931f95
ecollin2 <ecollin@seas.upenn.edu>
2025-03-24 18:29:18 -0600

    Update submitProof.py

--- submitProof.py
+++ submitProof.py
@@ -162,8 +162,8 @@ def send_signed_msg(proof, random_leaf):
         'gasPrice': w3.eth.gas_price
     })
 
-    signed_tx = w3.eth.account.sign_transaction(tx, private_key=acct.key)
-    tx_hash = w3.eth.send_raw_transaction(signed_tx['rawTransaction'])
+    signed_tx = acct.sign_transaction(tx)
+    tx_hash = w3.eth.send_raw_transaction(signed_tx.rawTransaction)
     print(f"Transaction sent with hash: {tx_hash.hex()}")
     return tx_hash.hex()
 
